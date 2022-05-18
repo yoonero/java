b04f87e67688eba3f6d414b49f5d7854fecbeba3
yoonero <105705268+yoonero@users.noreply.github.com>
2022-05-18 15:46:14 +0100

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    
+    // main method
+    public static void main(String[] args)
+    {
+        // print hello world
+        System.out.println("Hello World...");
+    }
+}
