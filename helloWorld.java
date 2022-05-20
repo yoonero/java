d43e684afb40243dea0e7a94d244696e1dd2cd6b
yoonero <105705268+yoonero@users.noreply.github.com>
2022-05-20 09:06:46 +0100

    Delete helloWorld.java

--- helloWorld.java
+++
@@ -1,9 +0,0 @@
-public class helloWorld {
-    
-    // main method
-    public static void main(String[] args)
-    {
-        // print hello world
-        System.out.println("Hello World...");
-    }
-}
