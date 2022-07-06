fe1ea47cd39c24f666d51a821c72b7b5e63c2a63
yoonero <105705268+yoonero@users.noreply.github.com>
2022-07-06 19:44:16 +0100

    Cmd calculator with =-/* operation

--- simpleCmdCalculator.java
+++ simpleCmdCalculator.java
@@ -8,15 +8,43 @@ public class simpleCmdCalculator
     {
         // variables 
         char operator;
-        double numberOne, numberTwo, result;
+        double numberOne, numberTwo;
 
         // scanner object 
         Scanner input = new Scanner(System.in);
 
+        //prompt first integer
+        System.out.print("Enter first Number:");
+        numberOne = input.nextInt();
+        
         // prompt for  operator
         System.out.println("select an operator + - * / ");
         operator = input.next().charAt(0);
 
+        //prompt second integer
+        System.out.print("Enter second Number:");
+        numberTwo = input.nextInt();
+        
+        if(operator == '+')
+        {
+            System.out.print(numberOne + numberTwo);
+        }
+        else if(operator == '-')
+        {
+            System.out.print(numberOne - numberTwo);
+        }
+        else if(operator == '*')
+        {
+            System.out.print(numberOne * numberTwo);
+        }
+        else if(operator == '/')
+        {
+            System.out.print(numberOne / numberTwo);
+        }
+        else 
+        {
+            System.out.print("no vaid input ");
+        }
         
     }    
 }
