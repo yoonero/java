82d7644ebcae6acc523d543e0360d5e7d3ad89ef
yoonero <105705268+yoonero@users.noreply.github.com>
2022-07-06 19:33:12 +0100

    Cmd calculat with basic operation

---
+++ simpleCmdCalculator.java
@@ -0,0 +1,22 @@
+// import java.util.Scanner
+import java.util.Scanner;
+
+public class simpleCmdCalculator
+{
+    // main method
+    public static void main(String[] args)
+    {
+        // variables 
+        char operator;
+        double numberOne, numberTwo, result;
+
+        // scanner object 
+        Scanner input = new Scanner(System.in);
+
+        // prompt for  operator
+        System.out.println("select an operator + - * / ");
+        operator = input.next().charAt(0);
+
+        
+    }    
+}
