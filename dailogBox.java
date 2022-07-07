99838e0f1ab5325a72cf4eba18c8d70288d1b5ae
yoonero <105705268+yoonero@users.noreply.github.com>
2022-07-07 15:15:32 +0100

    dialogbox

---
+++ dailogBox.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+// JOptionPane using 
+public  class dailogBox 
+{
+    // main method
+    public static void main(String[] args)
+    {
+        // dispay dialog with message 
+        JOptionPane.showMessageDialog(null, "welcome to java");
+    }// end main method
+}
